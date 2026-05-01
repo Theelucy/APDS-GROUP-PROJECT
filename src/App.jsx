e8c66c5adfa03d95d7231640aedf9e6613f85d14
Theelucy <159164608+Theelucy@users.noreply.github.com>
2026-05-01 23:10:00 +0200

    Add LandingPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
-
+import LandingPage from './pages/LandingPage.jsx'
 import AuthPage from './pages/AuthPage.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import PaymentPage from './pages/PaymentPage.jsx'
@@ -19,6 +19,7 @@ export default function App() {
           <Route path="/auth" element={<AuthPage />} />
 
           {/* Customer protected routes */}
+          <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard"     element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/payment"       element={<ProtectedRoute><PaymentPage /></ProtectedRoute>} />
           <Route path="/transactions"  element={<ProtectedRoute><TransactionsPage /></ProtectedRoute>} />
